9338b820dccaaba82224c548fc3534b69f3e2ef5
Abdullah Naeem <raoabdullahn@gmail.com>
2024-05-31 16:21:05 +0500

    Attach Mongodb Databse and use it to store blog post

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogsDB");
+
+
+const postSchema = new  mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Post = mongoose.model("Post", postSchema)
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,9 +27,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+// let posts = [];
 
-app.get("/", function(req, res){
+app.get("/", async function(req, res){
+  let posts = await Post.find();
+
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
@@ -37,31 +50,29 @@ app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const post = {
+app.post("/compose", async function(req, res){
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
+  });
 
-  posts.push(post);
+  await post.save();
 
   res.redirect("/");
 
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postID", async function(req, res){
+  const requestedPost = req.params.postID;
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
+  let foundPost = await Post.findById(requestedPost)
 
-    if (storedTitle === requestedTitle) {
+
       res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
+        title: foundPost.title,
+        content: foundPost.content
+      })
+  // console.log(requestedPost)    
 
 });
 
